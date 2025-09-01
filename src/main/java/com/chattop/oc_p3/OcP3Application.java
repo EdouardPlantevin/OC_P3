b9e75ebc8c5b6664b88372725f564ca0e817b784
EdouardPlantevin <plantevin.contact@gmail.com>
2025-09-01 17:36:16 +0200

    init project

---
+++ src/main/java/com/chattop/oc_p3/OcP3Application.java
@@ -0,0 +1,13 @@
+package com.chattop.oc_p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcP3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcP3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chattop/oc_p3/OcP3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chattop.oc_p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcP3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
